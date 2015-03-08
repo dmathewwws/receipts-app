5230d82b6b449cbda3b37e89b27b3e367cdcd18a
dannylmathews <dannyl.mathews@gmail.com>
2015-03-08 10:35:36 -0700

    Initial Commit

---
+++ Receipts++/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Receipts++
+//
+//  Created by Daniel Mathews on 2015-03-08.
+//  Copyright (c) 2015 com.theplayapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
